5f51d819f7a1851e8a7b55eec32219717adc6878
Wazed Biplob <www.biplob@gmail.com>
2023-05-02 00:38:52 +0600

    Header UI Completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,19 +5,25 @@ import Header from "../../Shared/Header";
 import { Button } from "react-bootstrap";
 const Home = () => {
   return (
-    <div className="banner d-flex align-items-center">
-      <div className="banner-text d-flex flex-column w-50 text-white gap-4 ms-4">
-        <h2>
-          <span style={{ color: "lightsalmon" }}>Chef</span> Master
-        </h2>
-        <h4>
-          Experience culinary excellence, Discover the art of flavor, Indulge in
-          the exquisite tastes, Savor every bite with creations, Elevate your
-          dining experience with english cuisine!
-        </h4>
-        <Button variant="secondary">Order Now</Button>{" "}
+    <>
+      <div className="banner">
+        <Header />
+        <div className="banner-text d-flex flex-column w-50 text-white gap-4 ms-4">
+          <h2>
+            Discover the art of
+            <span style={{ color: "lightsalmon" }}> flavour</span>
+          </h2>
+          <h5>
+            Experience culinary excellence, Indulge in the exquisite tastes,
+            Savor every bite with creations, Elevate your dining experience with
+            english cuisine!
+          </h5>
+          <Button className="w-50" variant="secondary">
+            Order Now
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="d-flex justify-content-between align-items-center p-4">
+      <div>
+        <h1 style={{ color: "lightsalmon" }}>Chef Master</h1>
+      </div>
+      <div className="d-flex gap-4">
+        <Link style={{ color: "lightcyan", textDecoration: "none" }} to="/">
+          Home
+        </Link>
+        <Link style={{ color: "lightcyan", textDecoration: "none" }} to="/blog">
+          Blog
+        </Link>
+      </div>
+      <div className="d-flex gap-4">
+        {/* <img src={user ? '' : ''} alt='user pic'/> */}
+        <Button variant="warning">Login</Button>
+        <Button variant="danger">Logout</Button>
+      </div>
     </div>
   );
 };
